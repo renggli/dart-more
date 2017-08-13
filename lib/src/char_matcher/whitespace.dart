713d54e6fa48cb42c9b652699ed9ae26450970cc
Lukas Renggli <renggli@gmail.com>
2017-08-13 17:35:08 +0200

    Switch is faster for whitespace matching.

--- lib/src/char_matcher/whitespace.dart
+++ lib/src/char_matcher/whitespace.dart
@@ -7,35 +7,37 @@ class WhitespaceCharMatcher extends CharMatcher {
 
   @override
   bool match(int value) {
-    if (value < 256) {
-      return value == 0x09 ||
-          value == 0x0A ||
-          value == 0x0B ||
-          value == 0x0C ||
-          value == 0x0D ||
-          value == 0x20 ||
-          value == 0x85 ||
-          value == 0xA0;
-    } else {
-      return value == 0x1680 ||
-          value == 0x180E ||
-          value == 0x2000 ||
-          value == 0x2001 ||
-          value == 0x2002 ||
-          value == 0x2003 ||
-          value == 0x2004 ||
-          value == 0x2005 ||
-          value == 0x2006 ||
-          value == 0x2007 ||
-          value == 0x2008 ||
-          value == 0x2009 ||
-          value == 0x200A ||
-          value == 0x2028 ||
-          value == 0x2029 ||
-          value == 0x202F ||
-          value == 0x205F ||
-          value == 0x3000 ||
-          value == 0xFEFF;
+    switch (value) {
+      case 0x09:
+      case 0x0A:
+      case 0x0B:
+      case 0x0C:
+      case 0x0D:
+      case 0x20:
+      case 0x85:
+      case 0xA0:
+      case 0x1680:
+      case 0x180E:
+      case 0x2000:
+      case 0x2001:
+      case 0x2002:
+      case 0x2003:
+      case 0x2004:
+      case 0x2005:
+      case 0x2006:
+      case 0x2007:
+      case 0x2008:
+      case 0x2009:
+      case 0x200A:
+      case 0x2028:
+      case 0x2029:
+      case 0x202F:
+      case 0x205F:
+      case 0x3000:
+      case 0xFEFF:
+        return true;
+      default:
+        return false;
     }
   }
 }
