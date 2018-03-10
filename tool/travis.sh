367ad7dce86c55ccaad61e42270077c148fcfb72
Lukas Renggli <renggli@gmail.com>
2018-03-10 13:39:49 +0100

    Cleanup travis configuration.

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,14 +3,15 @@
 # Fast fail the script on failures.
 set -e
 
-# Verify that the libraries are error free.
-dartanalyzer --fatal-warnings $(find . -name "*.dart")
-
-# Verify that all the tests pass.
-pub run test
+# Verify Dart 2.0 strict mode compatibility.
+if [ "${TRAVIS_DART_VERSION}" = "dev" ]; then
+  echo "Verifying Dart 2.0 strict mode ..."
+  dart --preview-dart-2 test/all_tests.dart
+fi
 
 # Verify the coverage of the tests.
 if [ "${COVERALLS_TOKEN}" ] && [ "${TRAVIS_DART_VERSION}" = "stable" ]; then
+  echo "Verifying test coverage ..."
   pub global activate dart_coveralls
   pub global run dart_coveralls report \
     --token "${COVERALLS_TOKEN}" \
