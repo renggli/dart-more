b1cbee8e7f2c0381b0d1c6fd5fb37219898474f0
Lukas Renggli <renggli@gmail.com>
2018-06-03 09:21:03 +0200

    Cleanup formatting.

--- lib/collection.dart
+++ lib/collection.dart
@@ -2,8 +2,8 @@ library more.collection;
 
 export 'package:more/src/collection/bimap.dart' show BiMap;
 export 'package:more/src/collection/bitlist.dart' show BitList;
-export 'package:more/src/collection/multiset.dart' show Multiset;
 export 'package:more/src/collection/double_range.dart' show DoubleRange;
 export 'package:more/src/collection/integer_range.dart' show IntegerRange;
+export 'package:more/src/collection/multiset.dart' show Multiset;
 export 'package:more/src/collection/string.dart'
     show string, StringList, mutableString, MutableStringList;
