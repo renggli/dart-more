e3b1737c04e88d595df302bf6fd1d89344779a0f
Lukas Renggli <renggli@gmail.com>
2018-08-24 11:55:27 +0200

    Messing with the number printer.

--- lib/src/printer/number_printer.dart
+++ lib/src/printer/number_printer.dart
@@ -5,8 +5,8 @@ import 'dart:math' as math;
 import '../../printer.dart';
 import 'utils.dart';
 
-/// Prints numbers in various formats.
-class NumberPrinter extends Printer {
+/// Prints numbers in a fixed format.
+class FixedNumberPrinter extends Printer {
   /// Round towards the nearest number that is a multiple of accuracy.
   final double _accuracy;
 
@@ -19,17 +19,14 @@ class NumberPrinter extends Printer {
   /// The delimiter to separate the integer and fraction part of the number.
   final String _delimiter;
 
-  /// The number of digits to be printed in the integer part.
-  final int _digits;
-
   /// The string that should be displayed if the number is infinite.
   final String _infinity;
 
   /// The string that should be displayed if the number is not a number.
   final String _nan;
 
-  /// The padding for the integer part.
-  final String _padding;
+  /// The way the sign of the number is printed.
+  final Printer _sign;
 
   /// The number of digits to be printed in the fraction part.
   final int _precision;
@@ -37,16 +34,15 @@ class NumberPrinter extends Printer {
   /// The separator character to be used to group digits.
   final String _separator;
 
-  NumberPrinter(
+  FixedNumberPrinter(
       this._accuracy,
       this._base,
       this._characters,
       this._delimiter,
-      this._digits,
       this._infinity,
       this._nan,
-      this._padding,
       this._precision,
+      this._sign,
       this._separator);
 
   @override
@@ -68,19 +64,13 @@ class NumberPrinter extends Printer {
   }
 
   String _convertInteger(num value) {
-    final digits = _intDigits(value.abs().truncate());
+    final digits = _intDigits(value.abs().truncate(), _base);
     return _convertIntegerDigits(digits);
   }
 
-  String _convertIntegerDigits(List<int> digits) {
-    var result = _formatDigits(digits);
-    if (_separator != null) {
-      result = _separateRight(result, _separator);
-    }
-    if (_digits != null && _padding != null) {
-      result = result.padLeft(_digits, _padding);
-    }
-    return result;
+  String _convertIntegerDigits(Iterable<int> digits) {
+    final result = _formatDigits(digits, _characters);
+    return _separator == null ? result : _separateRight(result, _separator);
   }
 
   String _convertFloat(num value) {
@@ -98,75 +88,142 @@ class NumberPrinter extends Printer {
   }
 
   String _convertFraction(double value) {
-    final digits = _intDigits(value.round());
-    var result = _formatDigits(digits).padLeft(_precision, _characters[0]);
-    if (_separator != null) {
-      result = _separateLeft(result, _separator);
-    }
-    return result;
+    final digits = _intDigits(value.round(), _base);
+    final result =
+        _formatDigits(digits, _characters).padLeft(_precision, _characters[0]);
+    return _separator == null ? result : _separateLeft(result, _separator);
   }
 
   String _convertBigInt(BigInt value) {
-    final digits = _bigIntDigits(value.abs());
+    final digits = _bigIntDigits(value.abs(), _base);
     return _convertIntegerDigits(digits);
   }
+}
 
-  List<int> _intDigits(int value) {
-    final digits = <int>[];
-    if (value == 0) {
-      digits.add(0);
-    } else {
-      value = value.abs();
-      while (value > 0) {
-        final next = value ~/ _base;
-        final index = value - next * _base;
-        digits.add(index);
-        value = next;
-      }
+class ScientificNumberPrinter extends Printer {
+  /// The numeric base to which the number should be printed.
+  final int _base;
+
+  /// The number of significant digits to be printed.
+  final int _significant;
+
+  /// The string separating the mantissa and exponent.
+  final String _notation;
+
+  /// The printer of the mantissa.
+  final Printer _mantissa;
+
+  /// The printer of the exponent.
+  final Printer _exponent;
+
+  ScientificNumberPrinter(
+      this._base,
+      String characters,
+      String delimiter,
+      String infinity,
+      String nan,
+      this._notation,
+      int precision,
+      String separator,
+      this._significant)
+      : _mantissa = Printer.sign() +
+            Printer.number(
+                base: _base,
+                characters: characters,
+                delimiter: delimiter,
+                infinity: infinity,
+                nan: nan,
+                precision: precision,
+                separator: separator),
+        _exponent = Printer.sign() +
+            Printer.number(
+                base: _base, characters: characters, separator: separator);
+
+  @override
+  String call(Object object) {
+    final value = checkNumericType(
+      object,
+      (value) => value.toDouble(),
+      (value) => value.toDouble(),
+    );
+    if (value.isInfinite || value.isNaN) {
+      return _mantissa(value);
     }
-    return digits;
-  }
-
-  List<int> _bigIntDigits(BigInt value) {
-    final digits = <int>[];
-    if (value == BigInt.zero) {
-      digits.add(0);
-    } else {
-      value = value.abs();
-      final base = BigInt.from(_base);
-      while (value > BigInt.zero) {
-        final next = value ~/ base;
-        final index = value - next * base;
-        digits.add(index.toInt());
-        value = next;
-      }
-    }
-    return digits;
-  }
-
-  String _formatDigits(List<int> digits) {
-    return digits.reversed.map((digit) => _characters[digit]).join();
-  }
-
-  String _separateLeft(String value, String separator) {
+    final exponent = _getExponent(value);
+    final mantissa = value / math.pow(_base, exponent);
     final buffer = StringBuffer();
-    for (var i = 0; i < value.length; i++) {
-      if (i != 0 && i % 3 == 0) {
-        buffer.write(separator);
-      }
-      buffer.writeCharCode(value.codeUnitAt(i));
-    }
+    buffer.write(_mantissa(mantissa));
+    buffer.write(_notation);
+    buffer.write(_exponent(exponent));
     return buffer.toString();
   }
 
-  String _separateRight(String value, String separator) {
-    final buffer = StringBuffer();
-    for (var i = 0; i < value.length; i++) {
-      if (i != 0 && (value.length - i) % 3 == 0) {
-        buffer.write(separator);
-      }
-      buffer.writeCharCode(value.codeUnitAt(i));
+  int _getExponent(double value) {
+    if (value == 0.0 || value == -0.0) {
+      return 0;
+    } else {
+      final logBase = math.log(value.abs()) / math.log(_base);
+      return logBase.floor() + 1 - _significant;
     }
-    return buffer.toString();
   }
+}
+
+/// Extracts digits of an [int] [value] in the provided [base].
+Iterable<int> _intDigits(int value, int base) {
+  if (value == 0) {
+    return [0];
+  }
+  final digits = <int>[];
+  while (value > 0) {
+    final next = value ~/ base;
+    final index = value - next * base;
+    digits.add(index);
+    value = next;
+  }
+  return digits.reversed;
+}
+
+/// Extracts digits of a [BitInt] [value] in the provided [base].
+Iterable<int> _bigIntDigits(BigInt value, int base) {
+  if (value == BigInt.zero) {
+    return [0];
+  }
+  final digits = <int>[];
+  final bigBase = BigInt.from(base);
+  while (value > BigInt.zero) {
+    final next = value ~/ bigBase;
+    final index = value - next * bigBase;
+    digits.add(index.toInt());
+    value = next;
+  }
+  return digits.reversed;
+}
+
+/// Converts an iterable of [digits] to a string using [characters] mapping.
+String _formatDigits(Iterable<int> digits, String characters) {
+  return digits.map((digit) => characters[digit]).join();
+}
+
+/// Separates an [input] string with a [separator] from the left.
+String _separateLeft(String input, String separator) {
+  final buffer = StringBuffer();
+  for (var i = 0; i < input.length; i++) {
+    if (i != 0 && i % 3 == 0) {
+      buffer.write(separator);
+    }
+    buffer.writeCharCode(input.codeUnitAt(i));
+  }
+  return buffer.toString();
+}
+
+/// Separates an [input] string with a [separator] from the right.
+String _separateRight(String input, String separator) {
+  final buffer = StringBuffer();
+  for (var i = 0; i < input.length; i++) {
+    if (i != 0 && (input.length - i) % 3 == 0) {
+      buffer.write(separator);
+    }
+    buffer.writeCharCode(input.codeUnitAt(i));
+  }
+  return buffer.toString();
 }
