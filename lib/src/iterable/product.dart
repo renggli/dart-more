1e601e8682642ccaad6bb814a27df03f993aad8d
Lukas Renggli <renggli@gmail.com>
2018-09-09 10:17:46 +0200

    Simplify the implementation of the product iterable.

--- lib/src/iterable/product.dart
+++ lib/src/iterable/product.dart
@@ -1,7 +1,5 @@
 library more.iterable.product;
 
-import 'dart:collection' show IterableBase;
-
 /// Returns an iterable over the cross product of [iterables].
 ///
 /// The resulting iterable is equivalent to nested for-loops. The rightmost
@@ -26,59 +24,31 @@ Iterable<List<E>> product<E>(Iterable<Iterable<E>> iterables) {
   if (iterables.isEmpty || iterables.any((iterable) => iterable.isEmpty)) {
     return const Iterable.empty();
   } else {
-    return ProductIterable<E>(iterables
+    return _product(iterables
         .map((iterable) => iterable.toList(growable: false))
         .toList(growable: false));
   }
 }
 
-class ProductIterable<E> extends IterableBase<List<E>> {
-  final List<List<E>> sources;
-
-  ProductIterable(this.sources);
-
-  @override
-  Iterator<List<E>> get iterator {
-    final state = List<int>.filled(sources.length, 0);
-    return ProductIterator<E>(sources, state);
-  }
-}
-
-class ProductIterator<E> extends Iterator<List<E>> {
-  final List<List<E>> sources;
-  final List<int> state;
-
-  bool completed = false;
-
-  ProductIterator(this.sources, this.state);
-
-  @override
-  List<E> current;
-
-  @override
-  bool moveNext() {
-    if (completed) {
-      return false;
-    }
-    if (current == null) {
-      current = List<E>.generate(sources.length, (i) => sources[i][0],
-          growable: false);
-      return true;
-    }
-    for (var i = state.length - 1; i >= 0; i--) {
-      if (state[i] + 1 < sources[i].length) {
-        state[i]++;
-        current[i] = sources[i][state[i]];
-        return true;
+Iterable<List<E>> _product<E>(List<List<E>> elements) sync* {
+  final indexes = List<int>.filled(elements.length, 0);
+  final current = List<E>.generate(elements.length, (i) => elements[i][0]);
+  var hasMore = false;
+  do {
+    yield current;
+    hasMore = false;
+    for (var i = indexes.length - 1; i >= 0; i--) {
+      if (indexes[i] + 1 < elements[i].length) {
+        indexes[i]++;
+        current[i] = elements[i][indexes[i]];
+        hasMore = true;
+        break;
       } else {
-        for (var j = state.length - 1; j >= i; j--) {
-          state[j] = 0;
-          current[j] = sources[j][0];
+        for (var j = indexes.length - 1; j >= i; j--) {
+          indexes[j] = 0;
+          current[j] = elements[j][0];
         }
       }
     }
-    completed = true;
-    current = null;
-    return false;
-  }
+  } while (hasMore);
 }
