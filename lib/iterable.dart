6f3dc34861496ec59b7e91e9cb6e16f44f38973b
Lukas Renggli <renggli@gmail.com>
2018-09-16 19:52:23 +0200

    Fix import order

--- lib/iterable.dart
+++ lib/iterable.dart
@@ -5,8 +5,8 @@ export 'package:more/src/iterable/combinations.dart' show combinations;
 export 'package:more/src/iterable/concat.dart' show concat;
 export 'package:more/src/iterable/cycle.dart' show cycle;
 export 'package:more/src/iterable/fold.dart' show fold;
-export 'package:more/src/iterable/indexed.dart' show indexed;
 export 'package:more/src/iterable/group.dart' show groupBy;
+export 'package:more/src/iterable/indexed.dart' show indexed;
 export 'package:more/src/iterable/iterate.dart' show iterate;
 export 'package:more/src/iterable/math.dart' show fibonacci, digits;
 export 'package:more/src/iterable/partition.dart'
