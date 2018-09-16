d780cff0d97b3e6b25b96f6180800ea2d6ee5848
Lukas Renggli <renggli@gmail.com>
2018-09-16 18:29:25 +0200

    Cleanup the partition iterable.

--- lib/src/iterable/partition.dart
+++ lib/src/iterable/partition.dart
@@ -1,34 +1,35 @@
 library more.iterable.partition;
 
-/// Divides an `iterable` into sub-iterables of a given `size`. The final
-/// iterable might be smaller or equal the desired size.
+/// Divides an `iterable` into sub-lists of a given `size`. The final list might
+/// be smaller or equal the desired size.
 ///
 /// The following expression yields [1, 2], [3, 4], [5]:
 ///
 ///     partition([1, 2, 3, 4, 5], 2);
+///
 Iterable<List<E>> partition<E>(Iterable<E> elements, int size) sync* {
-  final current = <E>[];
   final iterator = elements.iterator;
   while (iterator.moveNext()) {
+    final current = <E>[];
     do {
       current.add(iterator.current);
     } while (current.length < size && iterator.moveNext());
     yield current;
-    current.clear();
   }
 }
 
-/// Divides an `iterable` into sub-iterables of a given `size`. The final
-/// iterable is expanded with the provided `padding`.
+/// Divides an `iterable` into sub-lists of a given `size`. The final list is
+/// expanded with the provided `padding`.
 ///
 /// The expression yields [1, 2], [3, 4], [5, -1]:
 ///
 ///     partition([1, 2, 3, 4, 5], 2, -1);
+///
 Iterable<Iterable<E>> partitionWithPadding<E>(Iterable<E> elements, int size,
     [E padding]) sync* {
-  final current = <E>[];
   final iterator = elements.iterator;
   while (iterator.moveNext()) {
+    final current = <E>[];
     do {
       current.add(iterator.current);
     } while (current.length < size && iterator.moveNext());
@@ -36,6 +37,5 @@ Iterable<Iterable<E>> partitionWithPadding<E>(Iterable<E> elements, int size,
       current.add(padding);
     }
     yield current;
-    current.clear();
   }
 }
