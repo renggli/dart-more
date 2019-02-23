772275d48535d2af26bda808ce354801b7ca7ddc
Lukas Renggli <renggli@gmail.com>
2019-02-23 18:22:13 +0100

    Input to hyperbolic functions needs to be num, not double.

--- lib/src/math/hyperbolic.dart
+++ lib/src/math/hyperbolic.dart
@@ -5,29 +5,29 @@ import 'dart:math' as math;
 // Based on the polyfills given on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math.
 
 /// Computes the hyperbolic sine of a number.
-double sinh(double x) {
+double sinh(num x) {
   final y = math.exp(x);
   return (y - 1 / y) / 2;
 }
 
-/// Computes the hyperbolic arcsine of a number.
-double asinh(double x) =>
+/// Computes the hyperbolic arc-sine of a number.
+double asinh(num x) =>
     x.isInfinite && x.isNegative ? x : math.log(x + math.sqrt(x * x + 1));
 
 /// Computes the hyperbolic cosine of a number.
-double cosh(double x) {
+double cosh(num x) {
   final y = math.exp(x);
   return (y + 1 / y) / 2;
 }
 
-/// Computes the hyperbolic arccosine of a number.
-double acosh(double x) => math.log(x + math.sqrt(x * x - 1));
+/// Computes the hyperbolic arc-cosine of a number.
+double acosh(num x) => math.log(x + math.sqrt(x * x - 1));
 
 /// Computes the hyperbolic tangent of a number.
-double tanh(double x) {
+double tanh(num x) {
   final a = math.exp(x), b = math.exp(-x);
   return a.isInfinite ? 1 : b.isInfinite ? -1 : (a - b) / (a + b);
 }
 
-/// Computes the hyperbolic arctangent of a number.
-double atanh(double x) => math.log((1 + x) / (1 - x)) / 2;
+/// Computes the hyperbolic arc-tangent of a number.
+double atanh(num x) => math.log((1 + x) / (1 - x)) / 2;
