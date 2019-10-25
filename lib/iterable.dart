23a6de6e6da52024c044b51e17aed3122c27dfcb
Lukas Renggli <renggli@gmail.com>
2019-10-25 22:16:12 +0200

    Expose Group class.

--- lib/iterable.dart
+++ lib/iterable.dart
@@ -7,7 +7,7 @@ export 'package:more/src/iterable/combinations.dart' show combinations;
 export 'package:more/src/iterable/concat.dart' show concat;
 export 'package:more/src/iterable/cycle.dart' show cycle;
 export 'package:more/src/iterable/fold.dart' show fold;
-export 'package:more/src/iterable/group.dart' show groupBy;
+export 'package:more/src/iterable/group.dart' show groupBy, Group;
 export 'package:more/src/iterable/indexed.dart' show indexed, Indexed;
 export 'package:more/src/iterable/iterate.dart' show iterate;
 export 'package:more/src/iterable/math.dart' show fibonacci, digits;
