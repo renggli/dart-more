cec2e6a8f69a9104aff0bbfdc9e35a6e776ae1a8
Lukas Renggli <renggli@gmail.com>
2019-10-25 08:25:44 +0200

    Expose Indexed class.

--- lib/iterable.dart
+++ lib/iterable.dart
@@ -8,7 +8,7 @@ export 'package:more/src/iterable/concat.dart' show concat;
 export 'package:more/src/iterable/cycle.dart' show cycle;
 export 'package:more/src/iterable/fold.dart' show fold;
 export 'package:more/src/iterable/group.dart' show groupBy;
-export 'package:more/src/iterable/indexed.dart' show indexed;
+export 'package:more/src/iterable/indexed.dart' show indexed, Indexed;
 export 'package:more/src/iterable/iterate.dart' show iterate;
 export 'package:more/src/iterable/math.dart' show fibonacci, digits;
 export 'package:more/src/iterable/periodical.dart'
