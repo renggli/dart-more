177f960ef09c3129d7825b48bea74f6f379cb869
Lukas Renggli <renggli@gmail.com>
2020-08-29 13:14:16 +0200

    Create a parent package that imports all extensions at once.

---
+++ lib/more.dart
@@ -0,0 +1,12 @@
+/// More Dart — Literally.
+export 'package:more/async.dart';
+export 'package:more/cache.dart';
+export 'package:more/char_matcher.dart';
+export 'package:more/collection.dart';
+export 'package:more/hash.dart';
+export 'package:more/iterable.dart';
+export 'package:more/math.dart';
+export 'package:more/number.dart';
+export 'package:more/ordering.dart';
+export 'package:more/printer.dart';
+export 'package:more/tuple.dart';
