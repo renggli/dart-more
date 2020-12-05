e30b592c10b4ea62e81e8299d2a166f5dc802d18
Lukas Renggli <renggli@gmail.com>
2020-12-05 12:38:59 +0100

    Optimize reverse ComparatorOrdering.

--- lib/src/ordering/comparator.dart
+++ lib/src/ordering/comparator.dart
@@ -7,4 +7,19 @@ class ComparatorOrdering<T> extends Ordering<T> {
 
   @override
   int compare(T a, T b) => comparator(a, b);
+
+  @override
+  Ordering<T> get reversed => ReverseComparatorOrdering<T>(comparator);
+}
+
+class ReverseComparatorOrdering<T> extends Ordering<T> {
+  final Comparator<T> comparator;
+
+  const ReverseComparatorOrdering(this.comparator);
+
+  @override
+  int compare(T a, T b) => comparator(b, a);
+
+  @override
+  Ordering<T> get reversed => ComparatorOrdering<T>(comparator);
 }
