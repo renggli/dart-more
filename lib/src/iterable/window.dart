5409ff87c5e0b3a2b13ab17184e6cd9575279c24
Gergely KONTRA <gergely.kontra@icontest.hu>
2021-12-23 13:50:04 +0100

    Adding a window stream extension

--- lib/src/iterable/window.dart
+++ lib/src/iterable/window.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 extension WindowExtension<E> on Iterable<E> {
   /// Sliding window of given `size` over this [Iterable].
   ///
@@ -34,6 +36,43 @@ extension WindowExtension<E> on Iterable<E> {
           }
         }
       }
+    }
+  }
+}
+
+extension WindowStreamExtension<E> on Stream<E> {
+  Stream<List<E>> window(int size,
+      {int step = 1, bool includePartial = false}) async* {
+    if (size < 1) {
+      throw RangeError.value(size, 'size', 'size must be positive');
+    }
+    if (step < 1) {
+      throw RangeError.value(step, 'step', 'step must be positive');
+    }
+    int idx = 0; // to calculate step
+    final memory = ListQueue<E>(size);
+    await for (var elem in this) {
+      if (memory.length >= size) memory.removeFirst();
+      memory.addLast(elem);
+      if (memory.length == size) {
+        if (idx++ % step == 0) yield memory.toList(growable: false);
+      }
+    }
+
+    if (!includePartial || memory.isEmpty) return;
+
+    // first partial
+    if (idx == 0) {
+      yield memory.toList(growable: false);
+      idx++;
+    }
+
+    var rem = step - (idx - 1) % step;
+    var rest = memory.toList(growable: false);
+    while (rest.length > rem) {
+      rest = rest.sublist(rem);
+      yield rest.toList(growable: false);
+      rem = step;
     }
   }
 }
