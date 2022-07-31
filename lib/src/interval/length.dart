ee02deac63a85fb8c2e1638346363e89e1a41746
Lukas Renggli <renggli@gmail.com>
2022-07-31 20:32:48 +0200

    Improve comments and code clarity for Interval.toDoubleLength() and Interval.toIntLength().

--- lib/src/interval/length.dart
+++ lib/src/interval/length.dart
@@ -4,7 +4,7 @@ extension LengthIntervalNumExtension on Interval<num> {
   /// Returns the length an [Interval] as [int].
   ///
   /// Returns `0` if the interval is empty or `1` if the interval contains a
-  /// single value. Throws an [ArgumentError] if one of  the bounds is unbound
+  /// single value. Throws an [ArgumentError] if one of  the bounds is unbounded
   /// (infinite).
   int toIntLength() {
     if (isEmpty) {
@@ -24,11 +24,15 @@ extension LengthIntervalNumExtension on Interval<num> {
 
   /// Returns the length an [Interval] as [double].
   ///
-  /// Returns `0.0` if the interval is empty or `double.infinity` if one of
-  /// the bounds is unbound (infinite).
-  double toDoubleLength() => isEmpty
-      ? 0.0
-      : lower.isUnbounded || upper.isUnbounded
-          ? double.infinity
-          : upper.endpoint!.toDouble() - lower.endpoint!.toDouble();
+  /// Returns `0.0` if the interval is empty or contains a single value. Returns
+  /// `double.infinity` if one of the bounds is unbounded (infinite).
+  double toDoubleLength() {
+    if (isEmpty || isSingle) {
+      return 0.0;
+    } else if (lower.isUnbounded || upper.isUnbounded) {
+      return double.infinity;
+    } else {
+      return upper.endpoint!.toDouble() - lower.endpoint!.toDouble();
+    }
+  }
 }
