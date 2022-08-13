db7cac9e7002dc155bcd52d29d947074abe742cd
Lukas Renggli <renggli@gmail.com>
2022-08-13 18:10:45 +0200

    Improve documentation of Duration.convert

--- lib/src/temporal/duration/convert.dart
+++ lib/src/temporal/duration/convert.dart
@@ -2,10 +2,13 @@ import '../conversion.dart';
 import '../time_unit.dart';
 
 extension ConvertToDurationExtension on Duration {
-  /// Converts the [Duration] into a single [double] representations of the
-  /// provided [TimeUnit] `unit`.
+  /// Converts the [Duration] into a [double] representation of the provided
+  /// [TimeUnit] `unit`. The resulting duration has the same sign as this
+  /// duration.
   ///
-  /// The resulting duration has the same sign as this duration.
+  /// If you are not interested in the fractional part use the preferred
+  /// rounding ([double.round], [double.floor], [double.ceil],
+  /// [double.truncate]) to convert the result to an [int].
   ///
   /// By default a [casualConversion] is used, to get more accurate results
   /// use [accurateConversion] instead.
