61554fdd6b20d10434fcab5693d20b1b1e5f1b48
Lukas Renggli <renggli@gmail.com>
2022-09-19 22:34:41 +0200

    Avoid overflows when computing least common multiple (LCM).

--- lib/src/math/lcm.dart
+++ lib/src/math/lcm.dart
@@ -1,11 +1,11 @@
 extension LcmIntegerExtension on int {
   /// Returns the least common multiple (LCM) of this [int] and `other`. This is
   /// the smallest positive integer that is divisible by both numbers.
-  int lcm(int other) => this * other ~/ gcd(other);
+  int lcm(int other) => this ~/ gcd(other) * other;
 }
 
 extension LcmBigIntExtension on BigInt {
   /// Returns the least common multiple (LCM) of this [BigInt] and `other`. This
   /// is the smallest positive integer that is divisible by both numbers.
-  BigInt lcm(BigInt other) => this * other ~/ gcd(other);
+  BigInt lcm(BigInt other) => this ~/ gcd(other) * other;
 }
