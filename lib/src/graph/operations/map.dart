f3b39fc19f5c0c8b4ac88a001c54f361e2fe58dc
Lukas Renggli <renggli@gmail.com>
2022-12-25 17:13:36 +0100

    Optimize graph mapping.

--- lib/src/graph/operations/map.dart
+++ lib/src/graph/operations/map.dart
@@ -7,27 +7,24 @@ extension MapGraphExtension<V, E> on Graph<V, E> {
   /// The optional `vertex` function maps an edge
   Graph<VR, ER> map<VR, ER>({
     VR Function(V vertex)? vertex,
-    ER Function(Edge<V, E> edge)? edge,
+    ER? Function(Edge<V, E> edge)? edge,
   }) {
     final graph =
         isDirected ? Graph<VR, ER>.directed() : Graph<VR, ER>.undirected();
     final vertexMap = <V, VR>{};
+    final vertexMapper = vertex ?? (vertex) => vertex as VR;
     for (final oldVertex in vertices) {
-      final newVertex = vertex != null ? vertex(oldVertex) : oldVertex as VR;
+      final newVertex = vertexMapper(oldVertex);
       vertexMap[oldVertex] = newVertex;
       graph.addVertex(newVertex);
     }
+    final edgeMapper = edge ?? (edge) => edge.dataOrNull as ER?;
     for (final oldVertex in vertices) {
       for (final oldEdge in outgoingEdgesOf(oldVertex)) {
-        final newEdge = edge != null
-            ? edge(oldEdge)
-            : oldEdge.dataOrNull is ER
-                ? oldEdge.dataOrNull as ER
-                : null;
         graph.addEdge(
           vertexMap[oldEdge.source] as VR,
           vertexMap[oldEdge.target] as VR,
-          data: newEdge,
+          data: edgeMapper(oldEdge),
         );
       }
     }
