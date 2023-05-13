3cb440096fe0a8af171701c1df07267d140e2cab
Lukas Renggli <renggli@gmail.com>
2023-05-13 21:07:51 +0200

    Make `Either` and `Optional` actual sealed classes.

--- lib/src/functional/either.dart
+++ lib/src/functional/either.dart
@@ -1,8 +1,6 @@
 /// A container that may contain one or the other value.
 import 'package:meta/meta.dart';
 
-import 'either/left.dart';
-import 'either/right.dart';
 import 'optional.dart';
 import 'types/mapping.dart';
 
@@ -10,17 +8,15 @@ import 'types/mapping.dart';
 ///
 /// Also see https://en.wikipedia.org/wiki/Either_type.
 @immutable
-@sealed
-abstract class Either<L, R> {
+sealed class Either<L, R> {
   /// Internal const constructor.
-  @internal
-  const Either();
+  const Either._();
 
   /// Creates an [Either] with the left value populated.
-  const factory Either.left(L value) = LeftEither<L, R>;
+  const factory Either.left(L value) = LeftEither<L, R>._;
 
   /// Creates an [Either] with the right value populated.
-  const factory Either.right(R value) = RightEither<L, R>;
+  const factory Either.right(R value) = RightEither<L, R>._;
 
   /// Creates an left-sided [Either] from the [leftProvider], if [condition] is
   /// `true`. Otherwise, create a right-sided [Either] from the [rightProvider].
@@ -121,6 +117,144 @@ abstract class Either<L, R> {
 
   /// Returns a new [Either] with the left and right value switched.
   Either<R, L> swap();
+}
+
+/// Implementation of an [Either] with `leftValue` populated.
+class LeftEither<L, R> extends Either<L, R> {
+  const LeftEither._(this.leftValue) : super._();
+
+  @override
+  L get value => leftValue;
+
+  @override
+  final L leftValue;
+
+  @override
+  Optional<L> get leftOptional => Optional<L>.of(leftValue);
+
+  @override
+  R get rightValue => throw noRightValueError;
+
+  @override
+  Optional<R> get rightOptional => Optional<R>.absent();
+
+  @override
+  bool get isLeft => true;
+
+  @override
+  bool get isRight => false;
+
+  @override
+  T fold<T>(Map1<L, T> leftFunction, Map1<R, T> rightFunction) =>
+      leftFunction(leftValue);
+
+  @override
+  Either<L2, R2> map<L2, R2>(
+          Map1<L, L2> leftFunction, Map1<R, R2> rightFunction) =>
+      Either<L2, R2>.left(leftFunction(leftValue));
+
+  @override
+  Either<L2, R> mapLeft<L2>(Map1<L, L2> leftFunction) =>
+      Either<L2, R>.left(leftFunction(leftValue));
+
+  @override
+  Either<L, R2> mapRight<R2>(Map1<R, R2> rightFunction) =>
+      Either<L, R2>.left(leftValue);
+
+  @override
+  Either<L2, R2> flatMap<L2, R2>(Map1<L, Either<L2, R2>> leftFunction,
+          Map1<R, Either<L2, R2>> rightFunction) =>
+      leftFunction(leftValue);
+
+  @override
+  Either<L2, R> flatMapLeft<L2>(Map1<L, Either<L2, R>> leftFunction) =>
+      leftFunction(leftValue);
+
+  @override
+  Either<L, R2> flatMapRight<R2>(Map1<R, Either<L, R2>> rightFunction) =>
+      Either<L, R2>.left(leftValue);
+
+  @override
+  Either<R, L> swap() => Either<R, L>.right(leftValue);
+
+  @override
+  bool operator ==(Object other) =>
+      other is LeftEither && leftValue == other.leftValue;
+
+  @override
+  int get hashCode => Object.hash(LeftEither, leftValue);
+
+  @override
+  String toString() => '${super.toString()}[$leftValue]';
+}
+
+/// Implementation of an [Either] with `rightValue` populated.
+class RightEither<L, R> extends Either<L, R> {
+  const RightEither._(this.rightValue) : super._();
+
+  @override
+  R get value => rightValue;
+
+  @override
+  L get leftValue => throw noLeftValueError;
+
+  @override
+  Optional<L> get leftOptional => Optional<L>.absent();
+
+  @override
+  final R rightValue;
+
+  @override
+  Optional<R> get rightOptional => Optional<R>.of(rightValue);
+
+  @override
+  bool get isLeft => false;
+
+  @override
+  bool get isRight => true;
+
+  @override
+  T fold<T>(Map1<L, T> leftFunction, Map1<R, T> rightFunction) =>
+      rightFunction(rightValue);
+
+  @override
+  Either<L2, R2> map<L2, R2>(
+          Map1<L, L2> leftFunction, Map1<R, R2> rightFunction) =>
+      Either<L2, R2>.right(rightFunction(rightValue));
+
+  @override
+  Either<L2, R> mapLeft<L2>(Map1<L, L2> leftFunction) =>
+      Either<L2, R>.right(rightValue);
+
+  @override
+  Either<L, R2> mapRight<R2>(Map1<R, R2> rightFunction) =>
+      Either<L, R2>.right(rightFunction(rightValue));
+
+  @override
+  Either<L2, R2> flatMap<L2, R2>(Map1<L, Either<L2, R2>> leftFunction,
+          Map1<R, Either<L2, R2>> rightFunction) =>
+      rightFunction(rightValue);
+
+  @override
+  Either<L2, R> flatMapLeft<L2>(Map1<L, Either<L2, R>> leftFunction) =>
+      Either<L2, R>.right(rightValue);
+
+  @override
+  Either<L, R2> flatMapRight<R2>(Map1<R, Either<L, R2>> rightFunction) =>
+      rightFunction(rightValue);
+
+  @override
+  Either<R, L> swap() => Either<R, L>.left(rightValue);
+
+  @override
+  bool operator ==(Object other) =>
+      other is RightEither && rightValue == other.rightValue;
+
+  @override
+  int get hashCode => Object.hash(RightEither, rightValue);
+
+  @override
+  String toString() => '${super.toString()}[$rightValue]';
 }
 
 final noValueError = StateError('No value');
--- lib/src/functional/optional.dart
+++ lib/src/functional/optional.dart
@@ -1,8 +1,6 @@
 /// A container that may or may not contain a value.
 import 'package:meta/meta.dart';
 
-import 'optional/absent.dart';
-import 'optional/present.dart';
 import 'types/callback.dart';
 import 'types/mapping.dart';
 import 'types/predicate.dart';
@@ -14,17 +12,15 @@ import 'types/predicate.dart';
 ///
 /// Also see https://en.wikipedia.org/wiki/Option_type.
 @immutable
-@sealed
-abstract class Optional<T> {
+sealed class Optional<T> {
   /// Internal const constructor.
-  @internal
-  const Optional();
+  const Optional._();
 
   /// Returns a present [Optional] of the given [value].
-  const factory Optional.of(T value) = PresentOptional<T>;
+  const factory Optional.of(T value) = PresentOptional<T>._;
 
   /// Returns an absent [Optional] instance.
-  const factory Optional.absent() = AbsentOptional<T>;
+  const factory Optional.absent() = AbsentOptional<T>._;
 
   /// Returns a present [Optional] of the given [value] if the value is
   /// non-null, otherwise an absent [Optional] instance.
@@ -83,3 +79,117 @@ abstract class Optional<T> {
   /// [error] or a [StateError].
   T orElseThrow([Object? error]);
 }
+
+/// Implementation of an [Optional] with a value.
+class PresentOptional<T> extends Optional<T> {
+  const PresentOptional._(this.value) : super._();
+
+  @override
+  final T value;
+
+  @override
+  Iterable<T> get iterable => <T>[value];
+
+  @override
+  bool get isPresent => true;
+
+  @override
+  void ifPresent(Callback1<T> function) => function(value);
+
+  @override
+  bool get isAbsent => false;
+
+  @override
+  void ifAbsent(Callback0 function) {}
+
+  @override
+  Optional<T> where(Predicate1<T> function) =>
+      function(value) ? this : Optional<T>.absent();
+
+  @override
+  Optional<R> whereType<R>() =>
+      value is R ? Optional<R>.of(value as R) : Optional<R>.absent();
+
+  @override
+  Optional<R> map<R>(Map1<T, R> function) => Optional<R>.of(function(value));
+
+  @override
+  Optional<R> flatMap<R>(Map1<T, Optional<R>> function) => function(value);
+
+  @override
+  Optional<T> or(Map0<Optional<T>> function) => this;
+
+  @override
+  T orElse(T value) => this.value;
+
+  @override
+  T orElseGet(Map0<T> function) => value;
+
+  @override
+  T orElseThrow([Object? error]) => value;
+
+  @override
+  bool operator ==(Object other) =>
+      other is PresentOptional<T> && value == other.value;
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => '${super.toString()}[$value]';
+}
+
+/// Implementation of an [Optional] without a value.
+class AbsentOptional<T> extends Optional<T> {
+  const AbsentOptional._() : super._();
+
+  @override
+  T get value => throw noSuchElementError;
+
+  @override
+  Iterable<T> get iterable => const [];
+
+  @override
+  bool get isPresent => false;
+
+  @override
+  void ifPresent(Callback1<T> function) {}
+
+  @override
+  bool get isAbsent => true;
+
+  @override
+  void ifAbsent(Callback0 function) => function();
+
+  @override
+  Optional<T> where(Predicate1<T> function) => this;
+
+  @override
+  Optional<R> whereType<R>() => Optional<R>.absent();
+
+  @override
+  Optional<R> map<R>(Map1<T, R> function) => Optional<R>.absent();
+
+  @override
+  Optional<R> flatMap<R>(Map1<T, Optional<R>> function) => Optional<R>.absent();
+
+  @override
+  Optional<T> or(Map0<Optional<T>> function) => function();
+
+  @override
+  T orElse(T value) => value;
+
+  @override
+  T orElseGet(Map0<T> function) => function();
+
+  @override
+  T orElseThrow([Object? error]) => throw error ?? noSuchElementError;
+
+  @override
+  bool operator ==(Object other) => other is AbsentOptional<T>;
+
+  @override
+  int get hashCode => T.hashCode;
+}
+
+final noSuchElementError = StateError('No such element');
