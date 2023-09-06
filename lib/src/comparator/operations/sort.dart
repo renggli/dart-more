e64fe8b76eade4e944d618080125b2eaf80da103
Lukas Renggli <renggli@gmail.com>
2023-09-06 13:49:18 +0200

    Fix documentation of sort operator.

--- lib/src/comparator/operations/sort.dart
+++ lib/src/comparator/operations/sort.dart
@@ -3,12 +3,12 @@ import 'package:collection/collection.dart';
 extension SortComparator<T> on Comparator<T> {
   /// Sorts the provided [list] in-place.
   ///
+  /// If [start] (inclusive)  and [end] (exclusive) are provided the sorting
+  /// only happens within the specified range.
+  ///
   /// If [stable] is set to `true`, a stable merge-sort implementation is used
   /// instead of the standard quick-sort. This means that elements that compare
   /// equal stay in the order of the input.
-  ///
-  /// If [start] (inclusive)  and [end] (exclusive) are provided the sorting
-  /// only happens within the specified range.
   void sort(List<T> list, {int? start, int? end, bool stable = false}) {
     start ??= 0;
     end ??= list.length;
@@ -21,15 +21,18 @@ extension SortComparator<T> on Comparator<T> {
 
   /// Returns a sorted copy of the provided [iterable].
   ///
+  /// If [start] (inclusive)  and [end] (exclusive) are provided the sorting
+  /// only happens within the specified range.
+  ///
   /// If [stable] is set to `true`, a stable merge-sort implementation is used
   /// instead of the standard quick-sort. This means that elements that compare
   /// equal stay in the order of the input.
   ///
-  /// If [start] (inclusive)  and [end] (exclusive) are provided the sorting
-  /// only happens within the specified range.
+  /// By default a more efficient fixed-length list is returned, unless
+  /// [growable] is set to `true`.
   List<T> sorted(Iterable<T> iterable,
-      {int? start, int? end, bool stable = false}) {
-    final list = List.of(iterable, growable: false);
+      {int? start, int? end, bool stable = false, bool growable = false}) {
+    final list = List.of(iterable, growable: growable);
     sort(list, start: start, end: end, stable: stable);
     return list;
   }
