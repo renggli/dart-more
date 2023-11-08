fa9c0d90f9ecf04f8a6d7609fef5bf1e1a949858
Lukas Renggli <renggli@gmail.com>
2023-11-08 20:46:17 +0100

    Mask `bitCount` to enforce identical behavior on desktop and web.

--- lib/src/math/bit.dart
+++ lib/src/math/bit.dart
@@ -6,10 +6,10 @@ extension BitUint32Extension on int {
   ///
   /// See https://oeis.org/A000120.
   int get bitCount {
-    var n = this;
+    var n = this & 0xffffffff;
     n = n - ((n >> 1) & 0x55555555);
     n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
-    n = (n + (n >> 4)) & 0x0F0F0F0F;
+    n = (n + (n >> 4)) & 0x0f0f0f0f;
     n = n + (n >> 8);
     n = n + (n >> 16);
     return n & 0x0000003f;
