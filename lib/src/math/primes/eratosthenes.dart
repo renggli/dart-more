0b17f07d220362de8b4dc45bd3a0688ab90a3600
Lukas Renggli <renggli@gmail.com>
2024-11-07 13:26:46 +0100

    Minor optimization to the prime sieve of eratosthenes.

--- lib/src/math/primes/eratosthenes.dart
+++ lib/src/math/primes/eratosthenes.dart
@@ -10,11 +10,14 @@ import 'sieve.dart';
 class EratosthenesPrimeSieve extends PrimeSieve {
   /// Constructs the prime sieve of Eratosthenes.
   EratosthenesPrimeSieve(super.max) : _isPrime = BitList.filled(max + 1, true) {
-    if (max >= 0) _isPrime[0] = false;
-    if (max >= 1) _isPrime[1] = false;
-    for (var i = 2; i * i <= max; i++) {
+    _isPrime[0] = false;
+    if (max > 0) _isPrime[1] = false;
+    for (var i = 4; i <= max; i += 2) {
+      _isPrime[i] = false;
+    }
+    for (var i = 3; i * i <= max; i += 2) {
       if (_isPrime[i]) {
-        for (var j = i * i; j <= max; j += i) {
+        for (var j = i * i; j <= max; j += 2 * i) {
           _isPrime[j] = false;
         }
       }
