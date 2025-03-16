3402087bfe4462df3ccb1350450b291671a42a44
Lukas Renggli <renggli@gmail.com>
2025-03-16 08:31:18 +0100

    Improve code generation for unicode.

--- bin/generate_unicode.dart
+++ bin/generate_unicode.dart
@@ -7,11 +7,16 @@ import 'package:more/src/shared/rle.dart';
 import 'utils/generating.dart';
 import 'utils/unicode.dart';
 
+final whitepsaceSplitter = RegExp(r'\s+');
+
 void writeIntList(IOSink out, String name, List<int> values) {
   out.writeln('const $name = [');
   out.writeln(encodeRle(values).join(',\n'));
   out.writeln('];');
 }
+
+String formatUnicode(int codePoint) =>
+    '0x${codePoint.toRadixString(16).padLeft(4, '0')}';
 
 Future<void> generatePropertyData(String name, Uri url) async {
   final file = File('lib/src/char_matcher/unicode/$name.dart');
@@ -48,6 +53,111 @@ Future<void> generatePropertyData(String name, Uri url) async {
   await format(file);
 }
 
+Future<void> generateNormalizationData() async {
+  final file = File('lib/src/collection/string/normalization_data.dart');
+  final unicodeData = await unicodeDataFuture;
+  final compositionExclusions = await getLineData(compositionExclusionsUrl);
+  final out = file.openWrite();
+  generateWarning(out);
+
+  out.writeln('// $unicodeDataUrl');
+  out.writeln('// $compositionExclusionsUrl');
+  out.writeln();
+
+  out.writeln(
+    '/// Dictionary mapping characters with non-zero canonical combining class values',
+  );
+  out.writeln('/// to their corresponding values.');
+  out.writeln('const canonicalCombiningClasses = {');
+  for (final unicode in unicodeData) {
+    if (unicode.canonicalCombiningClasses != null &&
+        unicode.canonicalCombiningClasses != 0) {
+      out.write('${formatUnicode(unicode.codePoint)}: ');
+      out.writeln('${unicode.canonicalCombiningClasses},');
+    }
+  }
+  out.writeln('};');
+  out.writeln();
+
+  out.writeln(
+    '/// Dictionary mapping characters to their canonical decompositions.',
+  );
+  out.writeln('const characterDecompositionMapping = {');
+  for (final unicode in unicodeData) {
+    if (unicode.characterDecompositionMapping != null &&
+        unicode.characterDecompositionMapping!.isNotEmpty) {
+      out.write('${formatUnicode(unicode.codePoint)}: (');
+      var parts = unicode.characterDecompositionMapping!.split(
+        whitepsaceSplitter,
+      );
+      if (parts[0].startsWith('<')) {
+        out.write("type: '${parts[0]}', ");
+        parts = parts.sublist(1);
+      } else {
+        out.write('type: null, ');
+      }
+      out.write(
+        'values: [${parts.map((value) => formatUnicode(int.parse(value, radix: 16))).join(', ')}]),',
+      );
+    }
+  }
+  out.writeln('};');
+  out.writeln();
+
+  out.writeln('const compositionExclusion = {');
+  for (final exclusion in compositionExclusions) {
+    out.writeln('${formatUnicode(int.parse(exclusion[0], radix: 16))},');
+  }
+  out.writeln('};');
+  out.writeln();
+
+  await out.close();
+  await format(file);
+}
+
+Future<void> generateNormalizationTestData() async {
+  final file = File('test/data/normalization_data.dart');
+  final normalizationTestData = await getLineData(normalizationTestUrl);
+  final out = file.openWrite();
+  generateWarning(out);
+
+  out.writeln('// $normalizationTestUrl');
+  out.writeln();
+
+  out.writeln('const normalizationTestData = {');
+  for (final testData in normalizationTestData) {
+    if (testData.length == 1) {
+      if (testData != normalizationTestData.first) {
+        out.writeln('],');
+      }
+      out.writeln("'${testData[0]}': [");
+      continue;
+    }
+    final parts =
+        testData
+            .sublist(0, 5)
+            .map(
+              (part) => part
+                  .split(whitepsaceSplitter)
+                  .map((each) => formatUnicode(int.parse(each, radix: 16)))
+                  .join(', '),
+            )
+            .toList();
+    out.writeln('(');
+    out.writeln('source: [${parts[0]}],');
+    out.writeln('nfc: [${parts[1]}],');
+    out.writeln('nfd: [${parts[2]}],');
+    out.writeln('nfkc: [${parts[3]}],');
+    out.writeln('nfkd: [${parts[4]}],');
+    out.writeln('),');
+  }
+  out.writeln(']};');
+  out.writeln();
+
+  await out.close();
+  await format(file);
+}
+
 Future<void> generateUnicodeBlocks() async {
   final file = File('lib/src/char_matcher/unicode/blocks.dart');
   final data = await getPropertyData(unicodeBlocksUrl);
@@ -64,8 +174,7 @@ Future<void> generateUnicodeBlocks() async {
     final range = ranges.single;
     out.writeln(
       'const ${namify(key)} = RangeCharMatcher('
-      '0x${range.$1.toRadixString(16)}, '
-      '0x${range.$2.toRadixString(16)});',
+      '${formatUnicode(range.$1)}, ${formatUnicode(range.$2)});',
     );
   }
 
@@ -75,7 +184,7 @@ Future<void> generateUnicodeBlocks() async {
 
 Future<void> generateDecimalData() async {
   final file = File('lib/src/printer/number/numeral.dart');
-  final data = await unicodeData;
+  final unicodeData = await unicodeDataFuture;
   final out = file.openWrite();
   generateWarning(out);
 
@@ -100,7 +209,7 @@ Future<void> generateDecimalData() async {
   out.writeln('/// The default number system is [latin].');
   out.writeln('abstract class NumeralSystem {');
 
-  final iterator = data.iterator;
+  final iterator = unicodeData.iterator;
   while (iterator.moveNext()) {
     if (iterator.current.decimalDigit == 0) {
       final def = StringBuffer();
@@ -142,6 +251,8 @@ Future<void> main() => Future.wait([
   generatePropertyData('category', unicodeCategoryListUrl),
   generatePropertyData('property', unicodePropertyListUrl),
   generatePropertyData('bidi_class', unicodeBidiClassListUrl),
+  generateNormalizationData(),
+  generateNormalizationTestData(),
   generateUnicodeBlocks(),
   generateDecimalData(),
 ]);
